e16498ce77bc2ac0710517f89d2d7269f7b12be4
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-27 10:01:27 +0200

    remplir recettes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 // CSS
 import './App.css'
 import Header from "./components/Header";
+import recettes from './recettes'
+import Admin from "./components/Admin";
 
 class App extends Component {
-  state = {
-    pseudo: this.props.match.params.pseudo
-  }
+    state = {
+        pseudo: this.props.match.params.pseudo,
+        recettes: {}
+    }
 
-  render () {
-    return (
-      <div className='box'>
-        <Header pseudo={this.state.pseudo} />
-        <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
-        </div>
-      </div>
-    )
-  }
+    chargerExemple = () => this.setState({ recettes })
+
+    render() {
+        return (
+            <div className='box'>
+                <Header pseudo={this.state.pseudo}/>
+                <div className='cards'>
+                    <div className='card'>
+                        <h2>Une Carte</h2>
+                    </div>
+                </div>
+                <Admin
+                chargerExemple={this.chargerExemple}></Admin>
+            </div>
+        )
+    }
 }
 
 export default App
