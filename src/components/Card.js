90f332e2539feb37e4ba4579f72391fbaec8db44
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-27 13:29:41 +0200

    composant carte

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const Card = ({ details }) => {
+    const formatIngredients = details.ingredients
+        .split(",")
+        .map(item => <li key={item}>{item}</li>)
+
+    const formatInstructions = details.instructions
+        .split("\n")
+        .map(item => <li key={item}>{item}</li>)
+
     return (
-        <div className='card'>
+        <div className='card'>`
+            <div className='image'>
+                <img src={require(`../img/${details.image}`).default} alt={details.nom} />
+            </div>
             <div className='recette'>
                 <h2>{details.nom}</h2>
+                <ul className="liste-ingredients">
+                    {formatIngredients}
+                </ul>
+                <ol className="liste-instructions">
+                    {formatInstructions}
+                </ol>
             </div>
         </div>
     );
