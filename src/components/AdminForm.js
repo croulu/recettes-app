4972b773474e27df525d06a75d56a8d242e06050
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-01-31 17:27:58 +0100

    maj BDD

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const AdminForm = () => {
+const AdminForm = ({id: key, majRecette, recettes}) => {
+    const recette = recettes[key]
+
+    const handleChange = (event, key) => {
+        const {name, value} = event.target
+        const recette = recettes[key]
+        recette[name] = value
+        majRecette(key, recette)
+    }
+
     return (
         <div className='card'>
             <form className='admin-form'>
-                <input type='text' name='nom' placeholder="Nom de la recette" />
-                <input type='text' name='image' placeholder="Nom de l'image" />
-                <textarea name='ingredients' rows='3' placeholder="Liste des ingrédients" />
-                <textarea name='instructions' rows='15'  placeholder="Liste des instructions" />
+                <input type='text' value={recette.nom} onChange={e => handleChange(e, key)} name='nom'
+                       placeholder="Nom de la recette"/>
+                <input type='text' value={recette.image} onChange={e => handleChange(e, key)} name='image'
+                       placeholder="Nom de l'image"/>
+                <textarea value={recette.ingredients} onChange={e => handleChange(e, key)} name='ingredients' rows='3'
+                          placeholder="Liste des ingrédients"/>
+                <textarea value={recette.instructions} onChange={e => handleChange(e, key)} name='instructions'
+                          rows='15' placeholder="Liste des instructions"/>
             </form>
             <button>Supprimer</button>
         </div>
