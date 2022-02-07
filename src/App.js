ca4b19d97dae438b32f1e16de12f864a4be610aa
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-02-07 20:58:34 +0100

    React Context API

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import Admin from "./components/Admin";
 import Card from "./components/Card";
 import withFirebase from "./hoc/withFirebase";
 import PropTypes from "prop-types";
+import ColorContext from "./components/Color";
 
 const App = ({match, recettes, ajouterRecette, majRecette, supprimerRecette, chargerExemple}) => {
     const cards = Object.keys(recettes)
         .map(key => <Card key={key} details={recettes[key]}></Card>)
 
     return (
-        <div className='box'>
-            <Header pseudo={match.params.pseudo}/>
-            <div className='cards'>
-                {cards}
+        <ColorContext>
+            <div className='box'>
+                <Header pseudo={match.params.pseudo}/>
+                <div className='cards'>
+                    {cards}
+                </div>
+                <Admin
+                    pseudo={match.params.pseudo}
+                    recettes={recettes}
+                    ajouterRecette={ajouterRecette}
+                    majRecette={majRecette}
+                    supprimerRecette={supprimerRecette}
+                    chargerExemple={chargerExemple}
+                />
             </div>
-            <Admin
-                pseudo={match.params.pseudo}
-                recettes={recettes}
-                ajouterRecette={ajouterRecette}
-                majRecette={majRecette}
-                supprimerRecette={supprimerRecette}
-                chargerExemple={chargerExemple}
-            />
-        </div>
+        </ColorContext>
     )
 }
 
